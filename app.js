73fcc2fcfa48392e12d945ad40c3adaf5018b161
AKZHOL <46451454+akzho1@users.noreply.github.com>
2022-06-19 18:12:43 +0600

    variables

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+//1 Переменные
+// variable //
+const firstName = "name";
+const lastName = "lastName"; // string //
+let age = 26; // number //
+const isProgrammer = true; // boolean //
+
+console.log(firstName + "  " + lastName + "  1");
