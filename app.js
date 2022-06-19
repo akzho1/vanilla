905d416fb5e73e7409ff7d211d80ee9e02831ea6
AKZHOL <46451454+akzho1@users.noreply.github.com>
2022-06-19 19:05:46 +0600

    Data types

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //1 Переменные
 // variable //
-const firstName = "Name";
-const lastName = "lastName"; // string //
-// let age = 26; // number //
-const isProgrammer = true; // boolean //
-const _private = "name";
-const _ = "";
-const $ = "";
-// const if = ""; // error //
-const withNum5 = "5";
+// const firstName = "Name";
+// const lastName = "lastName"; // string //
+// // let age = 26; // number //
+// const isProgrammer = true; // boolean //
+// const _private = "name";
+// const _ = "";
+// const $ = "";
+// // const if = ""; // error //
+// const withNum5 = "5";
 // const 5withNum5 = "5"; // error
 
 
@@ -19,12 +19,12 @@ const withNum5 = "5";
 // alert(firstName + " " + lastNamee);
 
 //3 Операторы
-const currentYear = 2022;
-const birthYear = 1997;
-// const age = currentYear-birthYear;
-const a = 10;
-const b = 5;
-let c = 32;
+// const currentYear = 2022;
+// const birthYear = 1997;
+// // const age = currentYear-birthYear;
+// const a = 10;
+// const b = 5;
+// let c = 32;
 // c=c+a; одинаковые c+=a;
 // c+=a;
 // c-=a;
@@ -37,3 +37,14 @@ let c = 32;
 // console.log(a/b);
 // console.log(c);
 
+//4 Типы данных
+// const isProgrammer = true; // bolean
+// const firstName = "Programmer"; // string
+// const age = 25; // number
+// let x; // undefined
+// // null
+// console.log(typeof isProgrammer);
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof x);
+// console.log(null);
