2eb01a10b6d56e241bc79ad6a57e0599bc74c404
AKZHOL <46451454+akzho1@users.noreply.github.com>
2022-06-20 16:33:13 +0600

    ConditionalStatements

--- app.js
+++ app.js
@@ -57,3 +57,27 @@
 // // > < >= <= сравнение
 // const isFullAge = currentYear - birthYear >= fullAge; // 25>=26
 // console.log(isFullAge);
+
+// 6 Условные оператор
+// const courseStatus = "pending"; // ready, fail, pending
+
+// if (courseStatus === "ready") {
+//   console.log("Курс уже готов и его можно проходить")
+// } else if (courseStatus === "pending") {
+//   console.log("Курс находиться в процессе разработки")
+// } else {
+//   console.log("Курс не получился")
+// }
+const isReady = true;
+// if (isReady){
+//   console.log("Все готово!")
+// } else {
+//   console.log("Всё не готово!")
+// }
+// тернанрное выражение
+isReady ? console.log("Все готово!") : console.log("Всё не готово!");
+
+// const num1 = 42 // number число
+// const num2 = "42" // string строка
+
+// console.log(num1===num2) // всегда исползовать троейное = (===)
