c02125bd3d508fa0a7d22809a9f51ef4b4e30a39
AKZHOL <46451454+akzho1@users.noreply.github.com>
2022-06-20 17:14:10 +0600

    base

--- app.js
+++ app.js
@@ -68,16 +68,89 @@
 // } else {
 //   console.log("Курс не получился")
 // }
-const isReady = true;
-// if (isReady){
-//   console.log("Все готово!")
-// } else {
-//   console.log("Всё не готово!")
-// }
-// тернанрное выражение
-isReady ? console.log("Все готово!") : console.log("Всё не готово!");
+// const isReady = true;
+// // if (isReady){
+// //   console.log("Все готово!")
+// // } else {
+// //   console.log("Всё не готово!")
+// // }
+// // тернарное выражение
+// isReady ? console.log("Все готово!") : console.log("Всё не готово!");
 
 // const num1 = 42 // number число
 // const num2 = "42" // string строка
 
 // console.log(num1===num2) // всегда исползовать троейное = (===)
+
+// 7 Булевая логика (logic operators)
+// a1 = true  && true       // t && t вернёт true
+// a2 = true  && false      // t && f вернёт false
+// a3 = false && true       // f && t вернёт false
+// a4 = false && (3 == 4)   // f && f вернёт false
+// a5 = 'Cat' && 'Dog'      // t && t вернёт "Dog"
+// a6 = false && 'Cat'      // f && t вернёт false
+// a7 = 'Cat' && false      // t && f вернёт false
+// a8 = ''    && false      // f && f вернёт ""
+// a9 = false && ''         // f && f вернёт false
+
+// 8 функции
+// function calculateAge(year) {
+//   return 2022 - year;
+// }
+
+// // console.log(calculateAge(1997));
+// // console.log(calculateAge(1998));
+// // console.log(calculateAge(1999));
+// function logInfoAbout(name, year) {
+//   const age = calculateAge(year);
+//   if (age > 0) {
+//   console.log("Человек по имени "+ name + " сейчай имеет возраст " + age);
+//   } else {
+//     console.log("Вообщето это будущее")
+//   }
+// }
+// logInfoAbout("Name", 1997);
+// logInfoAbout("Jeka", 2021);
+
+// 9 Массивы
+// const cars = ["Мазда","Мерседес","Форд"];
+// // const cars = new Array("Мазда","Мерседес","Форд");
+// console.log(cars);
+// console.log(cars[0]);
+// console.log(cars[1]);
+// console.log(cars[2]);
+// console.log(cars.length); // длина массива
+// cars[0] = "Porshe";
+// // cars[3] = "Mazda"; // расширение массива
+// cars[cars.length] = "Mazda"; // расширение  массива
+// console.log(cars);
+
+// 10 циклы
+// const cars = ["Мазда","Мерседес","Форд","Porshe"];
+// // for (let i=0; i<cars.length; i++) {
+// //   const car = cars[i];
+// //   console.log(car);
+// // }
+// for (let car of cars) {
+//   console.log(car);
+// }
+
+// // 11 Объекты
+// const person = {
+//   firstName: "Jon",
+//   lastName: "Smith",
+//   year: "1998",
+//   languages: ["Ru","En","De"],
+//   hasWife: false,
+//   greet: function(){
+//     console.log("greet");
+//   }
+// }
+// console.log(person.firstName);
+// console.log(person["lastName"]);
+// const key = "languages";
+// console.log(person[key]);
+// person.hasWife=true;
+// person.isProgrammer=true;
+// console.log(person);
+// person.greet();
